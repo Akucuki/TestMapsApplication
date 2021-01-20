f0e0d2f6e210e938e6ffe5e5cf3d56d0ef3563d0
akucuki <pirko.vitaliy@gmail.com>
2021-01-20 18:40:38 +0200

    Changed default api url and request, map zoom value.

--- app/src/main/java/com/pirkovitaliysoft/testmapsapplication/presentation/common/Config.java
+++ app/src/main/java/com/pirkovitaliysoft/testmapsapplication/presentation/common/Config.java
@@ -1,6 +1,9 @@
 package com.pirkovitaliysoft.testmapsapplication.presentation.common;
 
 public class Config {
-    public static final String API_URL = "https://2fjd9l3x1l.api.quickmocker.com";
-    public static final float MAP_ZOOM = 0.5f;
+    //public static final String API_URL = "https://2fjd9l3x1l.api.quickmocker.com";
+    //public static final String API_REQUEST = "kyiv/places";
+    public static final String API_URL = "https://3lolo.github.io";
+    public static final String API_REQUEST = "lat_lng.json";
+    public static final float MAP_ZOOM = 15f;
 }
--- app/src/main/java/com/pirkovitaliysoft/testmapsapplication/presentation/model/PlacesContainerService.java
+++ app/src/main/java/com/pirkovitaliysoft/testmapsapplication/presentation/model/PlacesContainerService.java
@@ -1,11 +1,13 @@
 package com.pirkovitaliysoft.testmapsapplication.presentation.model;
 
+import com.pirkovitaliysoft.testmapsapplication.presentation.common.Config;
+
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PlacesContainerService {
-    @GET("kyiv/places")
+    @GET(Config.API_REQUEST)
     Call<PlacesContainer> listPlaces();
 }
